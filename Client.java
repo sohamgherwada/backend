f5645f421f8483d78a762cc00c5ce74b4d0a09e6
smallpop <william.tranviet@gmail.com>
2023-03-04 18:27:57 -0500

    SSL added

--- Client.java
+++ Client.java
@@ -1,82 +1,149 @@
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.PrintStream;
-import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.io.InputStream;
+import java.io.PrintStream;
 import java.util.Scanner;
 
-public class Client {
-    
-    final String host;
-    final int port;
 
+public class Client implements Runnable {
 
-    public static void main(String[] args) throws IOException{
+    private final String host;
+    private final int port;
+    private final String[] ciphersuites = // ALL THE CIPHER SUITES MINIMUM 256 BIT WE BALLLLLL
+            {"TLS_AES_256_GCM_SHA384",
+                    "TLS_CHACHA20_POLY1305_SHA256",
+                    "TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384",
+                    "TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256",
+                    "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384",
+                    "TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256",
+                    "TLS_DHE_RSA_WITH_AES_256_GCM_SHA384",
+                    "TLS_DHE_RSA_WITH_CHACHA20_POLY1305_SHA256",
+                    "TLS_DHE_DSS_WITH_AES_256_GCM_SHA384",
+                    "TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA384",
+                    "TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA384",
+                    "TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256",
+                    "TLS_DHE_RSA_WITH_AES_256_CBC_SHA256",
+                    "TLS_DHE_DSS_WITH_AES_256_CBC_SHA256",
+                    "TLS_ECDH_ECDSA_WITH_AES_256_GCM_SHA384",
+                    "TLS_ECDH_RSA_WITH_AES_256_GCM_SHA384",
+                    "TLS_ECDH_ECDSA_WITH_AES_256_CBC_SHA384",
+                    "TLS_ECDH_RSA_WITH_AES_256_CBC_SHA384",
+                    "TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA",
+                    "TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA",
+                    "TLS_DHE_RSA_WITH_AES_256_CBC_SHA",
+                    "TLS_DHE_DSS_WITH_AES_256_CBC_SHA",
+                    "TLS_ECDH_ECDSA_WITH_AES_256_CBC_SHA",
+                    "TLS_ECDH_RSA_WITH_AES_256_CBC_SHA",
+                    "TLS_RSA_WITH_AES_256_GCM_SHA384",
+                    "TLS_RSA_WITH_AES_256_CBC_SHA256",
+                    "TLS_RSA_WITH_AES_256_CBC_SHA",
+                    "TLS_EMPTY_RENEGOTIATION_INFO_SCSV"};
+
+    public static void main(String[] args) throws IOException {
         new Client("127.0.0.1", 5000).run();
     }
 
-    public Client(String host, int port){
+    public Client(String host, int port) {
         this.host = host;
         this.port = port;
     }
 
-    public void run() throws IOException{
-
-        Socket client = new Socket(host, port);
-        System.out.println("Connected to server!");
-        
-        PrintStream out = new PrintStream(client.getOutputStream());
-        
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("Nickname please: ");
-        String nick = sc.nextLine();
-
-        out.println(nick);
-
-        new Thread(new MHandler(client.getInputStream())).start();
-
-        System.out.println("Messages: \n");
-
-        while(sc.hasNextLine()){
-            out.println(sc.nextLine());
+    public void run() {
+        // connect client to server
+        SSLSocket client = null;
+        SSLSocketFactory factory = (SSLSocketFactory) SSLSocketFactory.getDefault();
+        try {
+            client = (SSLSocket) factory.createSocket(host, port);
+            client.setEnabledCipherSuites(ciphersuites); // only 256-bit ciphers and above
+            client.setUseClientMode(true); // force it to client mode
+            System.out.println("Starting handshake...");
+            client.startHandshake();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
+        System.out.println("Client successfully connected to server!");
+        // Get Socket output stream (where the client send her msg)
+        PrintStream output = null;
+        try {
+            output = new PrintStream(client.getOutputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(-1);
         }
 
-        out.close();
-        sc.close();
-        client.close();
+        // ask for a nickname
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a nickname: ");
+        String nickname = sc.nextLine();
 
+        // send nickname to server
+        output.println(nickname);
+
+        // create a new thread for server messages handling
+        try {
+            new Thread(new MHandle(client.getInputStream())).start();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
+
+        // read messages from keyboard and send to server
+        System.out.println("Messages: \n");
+
+        // while new messages
+        while (sc.hasNextLine()) {
+            output.println(sc.nextLine());
+        }
+
+        // end ctrl D
+        output.close();
+        sc.close();
+        try {
+            client.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
     }
 }
-class MHandler implements Runnable{
+
+class MHandle implements Runnable {
 
     private InputStream server;
 
-    public MHandler(InputStream server){
+    public MHandle(InputStream server) {
         this.server = server;
     }
 
-    public void run(){
-        Scanner sc = new Scanner(server);
+    public void run() {
+        // receive server messages and print out to screen
+        Scanner s = new Scanner(server);
         String tmp = "";
-        while(sc.hasNextLine()){
-            tmp = sc.nextLine();
-            if(tmp.charAt(0) == '['){
+        while (s.hasNextLine()) {
+            tmp = s.nextLine();
+            if (tmp.charAt(0) == '[') {
                 tmp = tmp.substring(1, tmp.length()-1);
-                System.out.println("\nUser list: " + new ArrayList<String>(Arrays.asList(tmp.split(","))) +"\n");
-            }
-            else{
-                try{
-                    System.out.println("\n" + tagVal(tmp));
-                }catch(Exception e){};
+                System.out.println(
+                        "\nUSERS LIST: " +
+                                new ArrayList<String>(Arrays.asList(tmp.split(","))) + "\n"
+                );
+            }else{
+                try {
+                    System.out.println("\n" + getTagValue(tmp));
+                    // System.out.println(tmp);
+                } catch(Exception ignore){}
             }
         }
-        sc.close();
+        s.close();
     }
-    public static String tagVal(String xml){
+
+    // I could use a javax.xml.parsers but the goal of Client.java is to keep everything tight and simple
+    public static String getTagValue(String xml){
         return  xml.split(">")[2].split("<")[0] + xml.split("<span>")[1].split("</span>")[0];
     }
+
 }
-
-
--- Server.java
+++ Server.java
@@ -1,102 +1,192 @@
-import java.io.*;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import javax.net.ServerSocketFactory;
+import javax.net.ssl.SSLServerSocket;
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLServerSocketFactory;
 
-public class Server {
+
+public class Server implements Runnable {
+
 
     private int port;
-    private ServerSocket server;
     private List<User> clients;
+    private SSLServerSocket server;
+    private final ServerSocketFactory sslSocketFactory = SSLServerSocketFactory.getDefault();
+    private String[] ciphersuites = // ALL THE CIPHER SUITES MINIMUM 256 BIT WE BALLLLLL
+            {"TLS_AES_256_GCM_SHA384",
+                    "TLS_CHACHA20_POLY1305_SHA256",
+                    "TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384",
+                    "TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256",
+                    "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384",
+                    "TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256",
+                    "TLS_DHE_RSA_WITH_AES_256_GCM_SHA384",
+                    "TLS_DHE_RSA_WITH_CHACHA20_POLY1305_SHA256",
+                    "TLS_DHE_DSS_WITH_AES_256_GCM_SHA384",
+                    "TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA384",
+                    "TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA384",
+                    "TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256",
+                    "TLS_DHE_RSA_WITH_AES_256_CBC_SHA256",
+                    "TLS_DHE_DSS_WITH_AES_256_CBC_SHA256",
+                    "TLS_ECDH_ECDSA_WITH_AES_256_GCM_SHA384",
+                    "TLS_ECDH_RSA_WITH_AES_256_GCM_SHA384",
+                    "TLS_ECDH_ECDSA_WITH_AES_256_CBC_SHA384",
+                    "TLS_ECDH_RSA_WITH_AES_256_CBC_SHA384",
+                    "TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA",
+                    "TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA",
+                    "TLS_DHE_RSA_WITH_AES_256_CBC_SHA",
+                    "TLS_DHE_DSS_WITH_AES_256_CBC_SHA",
+                    "TLS_ECDH_ECDSA_WITH_AES_256_CBC_SHA",
+                    "TLS_ECDH_RSA_WITH_AES_256_CBC_SHA",
+                    "TLS_RSA_WITH_AES_256_GCM_SHA384",
+                    "TLS_RSA_WITH_AES_256_CBC_SHA256",
+                    "TLS_RSA_WITH_AES_256_CBC_SHA",
+                    "TLS_EMPTY_RENEGOTIATION_INFO_SCSV"};
 
-    public Server(int port){
-        this.port = port;
-        this.clients = new ArrayList<>();
-    }
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         new Server(5000).run();
     }
 
-    public void run() throws IOException{
-        server = new ServerSocket(port);
+    public Server(int port) {
+        this.port = port;
+        this.clients = new ArrayList<>();
+
+    }
+
+    public void run() {
+        try{
+            server = (SSLServerSocket) sslSocketFactory.createServerSocket(port);
+            server.setEnabledCipherSuites(ciphersuites);
+            server.setUseClientMode(false);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
         System.out.println("Port " + port + " is now open.");
 
-        while(true){
-            Socket client = server.accept();
+        while (true) {
+            // accepts a new client
+            SSLSocket client = null;
+            try {
+                client = (SSLSocket) server.accept();
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.exit(0);
+            }
+            // get nickname of newUser
+            String nickname = null;
+            try {
+                nickname = (new Scanner( client.getInputStream() )).nextLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            System.out.println("New Client: \"" + nickname + "\"\nHost:" + client.getInetAddress().getHostAddress());
 
-            String nick = ( new Scanner(client.getInputStream() )).nextLine();
+            // create new User
+            User newUser = null;
+            try {
+                newUser = new User(client, nickname);
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.exit(0);
+            }
 
-            System.out.println("New client: " + nick + "\"\nHost:" + client.getInetAddress().getHostAddress());
-
-            User newUser = new User(client, nick);
-
+            // add newUser message to list
             this.clients.add(newUser);
 
-            newUser.getStreamOut().println("<b>User " + newUser +" has connected! </b>");
+            // Welcome msg
+            newUser.getOutStream().println("<b>Welcome</b> " + newUser + "<b>!</b>");
 
-            new Thread(new UHandle(this, newUser)).start();
+            // create a new thread for newUser incoming messages handling
+            new Thread(new UserHandler(this, newUser)).start();
         }
     }
 
-    public void remove(User user) {
+    // delete a user from the list
+    public void removeUser(User user){
         this.clients.remove(user);
     }
 
-    public void send(String msg, User sender){
-        for(User client: this.clients){
-            client.getStreamOut().println(sender.toString() + "<span>: " + msg+"</span>"); // THE SPANS ARE IMPORTANT
+    // send incoming msg to all Users
+    public void broadcastMessages(String msg, User userSender) {
+        for (User client : this.clients) {
+            client.getOutStream().println(
+                    userSender.toString() + "<span>: " + msg+"</span>");
+        }
+    }
+
+    // send list of clients to all Users
+    public void broadcastAllUsers(){
+        for (User client : this.clients) {
+            client.getOutStream().println(this.clients);
         }
     }
 }
 
-class UHandle implements Runnable{
+class UserHandler implements Runnable {
 
-    private final Server server;
-    private final User user;
+    private Server server;
+    private User user;
 
-    public UHandle(Server server, User user){
+    public UserHandler(Server server, User user) {
         this.server = server;
         this.user = user;
     }
 
-    public void run(){
-        String msg;
-        Scanner sc = new Scanner(this.user.getStreamIn());
-        while(sc.hasNextLine()){ // send message
-            msg = sc.nextLine();
-            server.send(msg, user);
+    public void run() {
+        String message;
+
+        // when there is a new message, broadcast to all
+        Scanner sc = new Scanner(this.user.getInputStream());
+        while (sc.hasNextLine()) {
+            message = sc.nextLine();
+            // broadcast the message to all users
+            server.broadcastMessages(message, user);
         }
-        server.remove(user); // kill thread
+        // end of Thread
+        server.removeUser(user);
         sc.close();
     }
 }
 
-class User{
-    private final PrintStream streamOut;
+class User {
+    private PrintStream streamOut;
     private InputStream streamIn;
-    private final String nick;
-    private final String color;
+    private String nickname;
+    private String color;
 
-    public User(Socket client, String name) throws IOException{
+    // constructor
+    public User(SSLSocket client, String name) throws IOException {
         this.streamOut = new PrintStream(client.getOutputStream());
         this.streamIn = client.getInputStream();
-        this.nick = name;
+        this.nickname = name;
         this.color = ColorInt.getColor((int) Math.round(Math.random()));
     }
-    public PrintStream getStreamOut(){
+
+
+    // getteur
+    public PrintStream getOutStream(){
         return this.streamOut;
     }
-    public InputStream getStreamIn(){
+
+    public InputStream getInputStream(){
         return this.streamIn;
     }
-    public String getNick(){
-        return this.nick;
+
+    public String getNickname(){
+        return this.nickname;
     }
+
+    // print user with his color
     public String toString(){
-        return "<u><span style='color:"+ this.color // THE COLORS ARE ESSENTIAL DO NOT
-                +"'>" + this.getNick() + "</span></u>";
+
+        return "<u><span style='color:" + this.color
+                +"'>" + this.getNickname() + "</span></u>";
+
     }
 }
 
